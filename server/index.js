dbf0f64ce33f3042af347123d533b03c9c61235b
beast-codez <lalithvedium456@gmail.com>
2025-06-19 17:27:09 +0530

    changed the directory of uploads

--- server/index.js
+++ server/index.js
@@ -11,8 +11,12 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 3001;
-
-app.use(cors());
+app.use(cors({
+    origin: 'https://file-compressor-hc.netlify.app', 
+    methods: ['GET', 'POST'],
+    credentials: true
+  }));
+  
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
